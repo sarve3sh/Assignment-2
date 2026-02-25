b90d48890738654a4ab5acf64365e932ab1b0716
Sarveshwaran Rajaraman <350366723@tdsb.ca>
2026-02-25 10:51:51 -0500

    changed CSS

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,1 +1,8 @@
 import '../style.css'
+/*three distinct real roots
+one real root and two complex roots
+one real root with a double root or a triple root*/
+
+/*Case A [Cardono's (1)+cube root of unity (2)]
+Case B [Cardono's+cube roots of unity(2)]
+Case C [Cardono's once]*/
