32fbfe91ac5f1e529b77978a17ab4e14a3ceca10
Sarveshwaran_Rajaraman <wsarvesh19@outlook.com>
2026-03-05 18:39:26 -0500

    Finished "use 1 of discriminant"

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -16,15 +16,20 @@ const d = parseFloat(valued. value);
 function solveCubic(a: number, b: number, c: number, d: number): number[]{
     const p = ((3*a*c-b**2)/(3*a**2))
     const q = ((27*a**2*d-9*a*b*c+2*b**3)/(27*a**3))
-    const discriminant = (q/2)**2 + (p/3)**2
+    const discriminant = (q/2)**2 + (p/3)**3
     if (discriminant>0){
-        //case a
+        const root = Math.cbrt(-q/2+Math.sqrt(discriminant)) + Math.cbrt(-q/2-Math.sqrt(discriminant)) - b/(3*a)
+        return [root];
     }
-    else if (discriminant==0){
-        //case b
+    else if (discriminant===0){
+        if (q===0 && p===0){
+        const root = -b/(3*a)
+        return [root,root,root];
+    }else{
+       const doubleRoot = 3*q/p-b/(3*a)
+       const singleRoot = -3*q/(2*p)-b/(3*a)
+       return [doubleRoot, doubleRoot, singleRoot];
     }
-    else if (discriminant<0){
-        //case c
-    }
-    return [];
+    } 
+    return[];
 }
